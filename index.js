27527b585a696798f51237838acc2278fb06ca6f
Teddy Minaya <teddyminaya@gmail.com>
2023-01-09 01:42:33 -0500

    commit tree 32156a9f4a6337c8a42b6b987d0cb973f8a64e72

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left);
+  dodger.style.left = `${left - 10}px`;
+}
+
+function moveDodgerRight() {
+  let right = parseInt(dodger.style.right);
+  dodger.style.right = `${right + 10}px`;
+}
